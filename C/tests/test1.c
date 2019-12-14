0ca4bbd2f62715c5a6261e794506f444bcf626ab
Iliass JABALI <i.jabali@aui.ma>
2019-12-14 17:51:54 +0000

    Update test1.c

--- C/tests/test1.c
+++ C/tests/test1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #define SIZE 100
 
 //Defining the struct
@@ -12,15 +11,25 @@ void add_student(student_t* library[], student_t* , int index);
 int search_by_id(student_t* library[], int index); 
 
 int main(void){
+  int keyword;
   //Declaring the library 
   student_t* library[SIZE];
   int index=0; //each elment has an index in the library 
   student_t s1={.id= 79945,.name= "iliass jabali"};
+  student_t s2={.id= 95163,.name= "full name"};
   add_student(library, &s1, index++);
+  add_student(library, &s2, index++);
   
-return 0;
+  return 0;
 }
 //adding book to library 
-void add_book(student_t* library[], student_t *bookp, int index){
-    library[index] = bookp;
+void add_book(student_t* library[], student_t *studentp, int index){
+   library[index] = studentp;
 }
+int search_by_id(student_t* library[], int index){
+   int i;
+   for (i = 0; i < index; i++)
+        if (contains_number(library[i]->title, keyword))
+            return i;
+    return -1;//-1 means it's not in the library 
+}  

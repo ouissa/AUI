d5590a39287a317ab0ff8b59aabc9bfd2277b666
Iliass JABALI <i.jabali@aui.ma>
2019-12-14 16:20:24 +0000

    Update test1.c

--- C/tests/test1.c
+++ C/tests/test1.c
@@ -1,18 +1,22 @@
-#include stdio.h 
-#include string.h
+#include <stdio.h>
+#include <string.h>
 #define SIZE 100
-//Defining the functions
+
+//Defining the struct
 typedef struct{
   int id;
   char* name;
-  int class;
 }student_t;
-void add_student(student_t* library[], int, char*, int); 
-char* search_by_id(student_t* library[], int); 
-
+//Defining the functions
+void add_student(student_t* library[], student_t* , int index); 
+int search_by_id(student_t* library[], int index); 
 
 int main(void){
-
-
+  //Declaring the library 
+  student_t* library[SIZE];
+  int index=0; //each elment has an index in the library 
+  student_t s1={.id= 79945,.name= "iliass jabali"};
+  add_student(library, &s1, index++);
+  print
 return 0;
 }

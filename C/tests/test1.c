a6eebf12e9c19ef9f0bfd743853a1209ee5ba22f
Iliass JABALI <i.jabali@aui.ma>
2019-12-14 14:39:51 +0000

    Create test1.c

---
+++ C/tests/test1.c
@@ -0,0 +1,18 @@
+#include stdio.h 
+#include string.h
+#define SIZE 100
+//Defining the functions
+typedef struct{
+  int id;
+  char* name;
+  int class;
+}student_t;
+void add_student(student_t* library[], int, char*, int); 
+char* search_by_id(student_t* library[], int); 
+
+
+int main(void){
+
+
+return 0;
+}
